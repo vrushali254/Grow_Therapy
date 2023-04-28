e3bd889205635db139611aa50df82da9b1c6d1b1
vrushali254 <vrushali254@gmail.com>
2023-04-28 09:57:21 -0700

    Added Readme file, artifact

--- src/main/java/com/analytics/AnalyticsApplication.java
+++ src/main/java/com/analytics/AnalyticsApplication.java
@@ -2,12 +2,13 @@ package com.analytics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AnalyticsApplication {
+public class AnalyticsApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Starting the analytics application. Enjoy!");
         SpringApplication.run(AnalyticsApplication.class, args);
     }
 }
