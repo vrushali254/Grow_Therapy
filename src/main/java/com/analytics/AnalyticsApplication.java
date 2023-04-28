2807d9bd322adad1139b4e3d2d4e968e8c07802c
vrushali254 <vrushali254@gmail.com>
2023-04-28 10:58:58 -0700

    Added executable artifact

--- src/main/java/com/analytics/AnalyticsApplication.java
+++ src/main/java/com/analytics/AnalyticsApplication.java
@@ -2,9 +2,10 @@ package com.analytics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AnalyticsApplication {
+public class AnalyticsApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
 
         System.out.println("Starting the analytics application. Enjoy!");
